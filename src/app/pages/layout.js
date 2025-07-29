0ed2f0f7cd9ce9b4b56861c6044eb1abbe389d8a
ShanoorAbdul-cyber <abdulshanoor69@gmail.com>
2025-07-29 17:19:06 +0530

    Removed console

--- src/app/pages/layout.js
+++ src/app/pages/layout.js
@@ -3,6 +3,5 @@
 import AuthGuard from "../components/AuthGuard";
 
 export default function PagesLayout({ children }) {
-  console.log("children",children)
   return <AuthGuard>{children}</AuthGuard>;
 }
